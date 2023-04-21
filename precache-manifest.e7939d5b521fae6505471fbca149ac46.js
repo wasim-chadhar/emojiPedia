ba4e6b5828b4a5b1feabcd3e8394134dd47ae49e
Muhammad Wasim Munir <2019cs624@student.uet.edu.pk>
2023-04-21 11:39:54 +0500

    Updates

---
+++ precache-manifest.e7939d5b521fae6505471fbca149ac46.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda0f3db10758206b08e4f475b7c4e4e",
+    "url": "/emojiPedia/index.html"
+  },
+  {
+    "revision": "6b6834979d5b1cb046d2",
+    "url": "/emojiPedia/static/js/2.c38b2fb2.chunk.js"
+  },
+  {
+    "revision": "ea7d668ae3e2fd0f84ca",
+    "url": "/emojiPedia/static/js/main.b7060b61.chunk.js"
+  },
+  {
+    "revision": "6d475a0bb0b211d7333b",
+    "url": "/emojiPedia/static/js/runtime-main.f9f1e4af.js"
+  }
+]);
